e060264e38649de27baeb41c554c3d002306b5a9
arkdevuk <florian.chambon-lacoste@weemars.fr>
2022-10-10 12:29:09 +0200

    added base recipe, WIP

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <div v-if="userData !== undefined" class="w-100">
+        <h3>Formulaire contact</h3>
+        <form action="">
+            <input v-model="userData.name" type="text">
+        </form>
+    </div>
+
+    <div class="sepatron x"></div>
+
+    <div class="w-100 listing-recipes">
+        <recipe v-for="recipe in recipesList"
+                :key="recipe.id"
+                :recipe="recipe"></recipe>
+    </div>
+
+    <HelloWorld :user="userData"
+                msg="Macaque 1"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue';
+    import Recipe from './components/UI/recipes/recipe.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    const user_ = {
+        name: 'John',
+        age: 30,
+        address: {
+            street: 'Main Street',
+            number: 123
+        }
+    };
+
+    const recipesList_ = [
+        {
+            id: 3,
+            title: 'BigMac',
+            review: 3,
+            creator: 'Ronald McDonald',
+        },
+        {
+            id: 1,
+            title: 'Pumpkin Soup',
+            review: 4.7,
+            creator: 'Harry Potter',
+        },
+        {
+            id: 2,
+            title: 'Fried Chicken',
+            review: 4.7,
+            creator: 'Colonel Sanders',
+        },
+        {
+            id: 4,
+            title: 'Double SteakHouse',
+            review: 5,
+            creator: 'BK',
+        }
+    ];
+
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld,
+            Recipe,
+        },
+        data() {
+            return {
+                d: null,
+                query: 'default :D',
+                userData: undefined,
+                recipesList: recipesList_,
+            }
+        },
+        mounted() {
+            setTimeout(() => {
+                this.userData = user_;
+            }, 2000);
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    div.sepatron {
+        margin: 20px 0;
+    }
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/UI/recipes/recipe.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="recipe">
+        <div v-if="recipe !== undefined" class="w-100">
+            <div class="w-100">
+                <img alt="recipe illustration"
+                     src="https://via.placeholder.com/350x150">
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <h3>{{ recipe.title }}</h3>
+                    <div class="review">⭐ {{ recipe.review }}</div>
+                </div>
+                <div class="col-6">
+                    <p class="text-muted">by {{ recipe.creator }}</p>
+                </div>
+            </div>
+        </div>
+        <div v-else class="w-100">
+            <p>chargement ...</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "recipe",
+        props: {
+            recipe: Object,
+        },
+        data() {
+            return {
+                d: null,
+            }
+        },
+        watch: {},
+        computed: {},
+        created() {
+
+        },
+        mounted() {
+
+        },
+        methods: {},
+        components: {},
+        beforeUnmount() {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
